e6d8131e8b7bb77aace32263d63e2571937605b5
James Lin <james@JamestekiMac-mini.local>
2019-02-15 00:36:59 +0800

    submit install-openshift

--- install-openshift.sh
+++ install-openshift.sh
@@ -1,40 +1,54 @@
 #/bin/sh
+:set ts=4 st=4 sw=4 et cul smarttab
 
 DOCKER_DAEMON_JSON=/etc/docker/daemon.json
-#OC_URL="https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz"
 OC_URL="https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz"
 OC_FILENAME_TARGZ=`basename ${OC_URL}`
-IP_ADDRESS=`ip address show|grep 'ens' | grep 'inet'|awk '{print $2}'|sed 's/\/.*//g'`
+IP_ADDRESS=`cat /etc/sysconfig/network-scripts/ifcfg-*| 
+    grep IPADDR | 
+    grep -v '127.0.0.1' |
+    awk -F= '{print $2}' |
+    sed "s/\"//g"`
+
 DOCKER_CE_REPO=/etc/yum.repos.d/docker-ce.repo
 OC_BASE_DIR=/opt/oc/
 OC_CLUSTER_UP=/usr/local/sbin/oc_cluster_up.sh
 mkdir -p $OC_BASE_DIR
+SELINUX=/etc/selinux/config
 
 date
 #=======================================================
 echo "## Step 1:  OS version confirm: "
+echo "## Only for CentOS Linux release 7.5.1804 (Core) and later"
+echo "System:"
 uname -a
 cat /etc/redhat-release
 echo
+read -p "Press Enter to install or CTRL-C to exit"
+
+echo "## Disable selinux first"
+for SETTING in enforcing permissive ; do
+    if grep "SELINUX=${SETTING}" $SELINUX ; then
+        echo "Please reboot server once for SELINUX disabled, now."
+        echo "And run this script again."
+        sed -i -e "s/SELINUX=${SETTING}/SELINUX=disabled/" $SELINUX
+        echo "## Configure:"
+        grep "SELINUX=disabled" $SELINUX
+        exit 0
+    fi
+done
 
 # install packages
-echo "## install required packeds: yum-utils device-mapper-persistent-data lvm2 screen git"
+echo "## Install required packeds: yum-utils device-mapper-persistent-data lvm2 screen git"
+sudo yum clean all
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2 \ 
   screen \ 
   git
 
-# Install docker-ce-edge
-#sudo yum-config-manager \
-#    --add-repo \
-#    https://download.docker.com/linux/centos/docker-ce.repo
-# Docker installer
-#echo "## Install docker "
-#yum -y install docker
-#echo
-# add docker-ce edge repo to yum repository
-echo >${DOCKER_CE_REPO} <<EOF
+echo  "## Add docker-ce edge repo to yum repository"  
+cat >${DOCKER_CE_REPO} <<'EOF'
 [docker-ce-edge]
 name=Docker CE Edge - $basearch
 baseurl=https://download.docker.com/linux/centos/7/$basearch/edge
@@ -44,11 +58,24 @@ gpgkey=https://download.docker.com/linux/centos/gpg
 EOF
 cat ${DOCKER_CE_REPO}
 echo
-echo "## install docker-ce-edge"
+
+echo "## Remove old default docker"
+if rpm -qa | grep docker-1 ; then
+    yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+fi
+
+echo "## Install docker-ce-edge"
 yum -y install docker-ce
-echo "## systemctl start docker"
+echo "## Systemctl start docker"
 systemctl start docker
-echo "## systemctl enable docker autostart"
+echo "## Systemctl enable docker start on boot"
 systemctl enable docker
 
 date
@@ -78,19 +105,21 @@ EOF
 cat ${DOCKER_DAEMON_JSON}
 echo
 
-echo "## systemctl daemon-reload"
+echo "## Systemctl daemon-reload"
 systemctl daemon-reload
 echo
-echo "## systemctl restart docker"
+echo "## Systemctl restart docker"
 systemctl restart docker
 
-echo "## confirm docker version"
+echo "## Confirm docker version"
 docker version
 if $? != 0; then
     echo "Can not check docker version."
     echo "Please install docker first."
     exit 1
 fi
+echo "## Check docker running stat"
+systemctl status docker
 echo
 
 date
@@ -104,6 +133,8 @@ sudo systemctl disable firewalld
 # firewall-cmd --permanent --zone dockerc --add-port 53/udp
 # firewall-cmd --permanent --zone dockerc --add-port 8053/udp
 # firewall-cmd --reload
+echo -e "firewall stat is \c"
+firewall-cmd --stat
 echo
 
 date
@@ -122,9 +153,10 @@ echo
 date
 #=======================================================
 echo "## Step 6: start up oc cluster"
-#oc cluster up --host-data-dir="/opt"
+echo "## command:"
+echo "oc cluster up --public-hostname=${IP_ADDRESS}  --base-dir=${OC_BASE_DIR}"
 oc cluster up --public-hostname=${IP_ADDRESS} --base-dir=${OC_BASE_DIR}
-cat >${OC_CLUSTER_UP} <<EOF
+cat >>${OC_CLUSTER_UP} <<'EOF'
 LOCAL_IP=$(ip address show | grep 'ens' |grep 'inet ' |awk '{print $2}' |awk -F/ '{print $1}' )
 BASE_DIR=/opt/oc
 OC=/usr/local/bin/oc
@@ -141,6 +173,6 @@ docker images
 
 date
 #=======================================================
-echo "## Step 7: oc login -u developer"
-oc login -u developer
+echo "## Step 7: oc login -u system:admin"
+oc login -u system:admin
 echo "## Finished."
