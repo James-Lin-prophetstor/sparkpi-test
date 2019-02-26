fd4ff4496135e71f189f43f7df63322f7871a8be
james.lin <james.lin@prophetstor.com>
2019-02-26 16:00:46 +0800

    monitor docker in a project

--- oc_project_monitor.py
+++ oc_project_monitor.py
@@ -1,13 +1,23 @@
 # -*- coding: UTF-8 -*-
+"""
+Query CPU and MEMORY usages of the containers in a openshift project.
+Like promethues monitoring.
+"""
 import os
 import StringIO
 import subprocess
 import time
 import json
 
-Project = raw_input("Project Name: ")
 User = raw_input("Project user: ")
 Password = raw_input("User password: ")
+# Test to login oc into the project
+CMD = 'oc login -u '+User+' -p '+Password
+subprocess.check_call([CMD],shell=True)
+Project = raw_input("Project Name: ")
+CMD = 'oc project '+Project
+subprocess.check_call([CMD],shell=True)
+# Set parameters
 Interval = int(input("Set interval for logger (minutes): "))
 Script_Duration = int(input("Set how long will in monitoring (miutes): "))
 Date = time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
@@ -30,7 +40,9 @@ OC_pods = subprocess.Popen([CMD], stdout=subprocess.PIPE,
 List_pod = []
 for Pod in OC_pods:
     List_pod.append(Pod.split("/")[1])
-    
+print("pod in project")
+print(List_pod)
+print("*"*20)
 # get docker ID's and NAME's mapping
 CMD = 'docker ps --format "{{.ID}}\t{{.Names}}"'
 DOCKERS = os.popen(CMD).read()
@@ -39,30 +51,39 @@ for ID in StringIO.StringIO(DOCKERS):
     items = ID.split()
     Key, Value = items[0], items[1]
     DICT_ID[Key] = Value
-
+print("Dokcer ID and Name of all")
+print(DICT_ID)
+print("*"*20)
 DICT_POD_IN_PROJECT = {}
 for ID, Name in DICT_ID.items():
     for Pod in List_pod:
         if Pod in Name:
             DICT_POD_IN_PROJECT[ID] = Name
-print DICT_POD_IN_PROJECT.keys()
-
+print("POD ID & Name in project" )
+print(json.dumps(DICT_POD_IN_PROJECT))
+print("*"*20)
 # get docker stats for pods in the project
 Test_times = int(Script_Duration / Interval)
 for count in range(0, Test_times, 1):
     CMD = 'docker  stats --all --no-stream --format "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}"'
     DOCKERS = os.popen(CMD).read()
     Date = str(time.strftime("%Y-%m-%d-%H%M%S", time.localtime()))
-    Text = {}
-    Text[Date] = "Container      CPUPerc      MemUsage"
-    for Docker in StringIO.StringIO(DOCKERS):
-        ID = Docker.split()[0]
-        if ID in DICT_POD_IN_PROJECT.keys():
-            Text[ID] = Date +" " + DICT_POD_IN_PROJECT[ID] + " " + Docker        
-    print "="*20
-    for Key in Text.keys():
-        print(Key, Text[Key])
-        log = open(LOG, "a")
-        log.write(str(Key), str(Text[Key])
-        log.close()
+    Text = " "
+    Text= "Date    Container    ID      CPUPerc      MemUsage \n"
+    with open(LOG,"a") as f:
+        f.write(Text)
+    print("Dokkers status in the project: ", Project)
+    with open(LOG, "a") as f:
+        for Docker in StringIO.StringIO(DOCKERS):
+            ID = Docker.split()[0]
+            if ID in DICT_POD_IN_PROJECT.keys():
+                Text = Date+" "+DICT_POD_IN_PROJECT[ID]+" "+Docker
+                print(Text)
+                f.write(Text)
+        f.close()
+    print("*"*20)
     time.sleep(Interval*60)
+print("Monitor done. log file is: " LOG)
+print("\n")
+
+
